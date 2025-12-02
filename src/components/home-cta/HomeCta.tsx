7ffd56eccdaa7af51f211bb55a2bee3c6dc20573
Manjinder Singh <smanjinder576@gmail.com>
2025-12-02 23:14:55 +0530

    fix link

--- src/components/home-cta/HomeCta.tsx
+++ src/components/home-cta/HomeCta.tsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom"
+
 export const HomeCta = () => {
     return <div className="font-bold text-slate-800">
         Who do you think is the biggest aura farmer in the world?
         Is it Piccolo? or Michael Jackson? or someone else?
         <div className="flex items-center gap-2 justify-center">
-            <a href="/aura-farmers" className="text-white bg-green-700 px-4 py-2 rounded-lg">Vote Now</a>
+            <Link to="/aura-farmers" className="text-white bg-green-700 px-4 py-2 rounded-lg">Vote Now</Link>
         </div>
     </div>
 }
